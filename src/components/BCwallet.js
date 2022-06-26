2e4ed70a00a11524f3b627798fc42971053658db
金下裕平 <yuheikaneshita@kinkayuuheis-MacBook-Pro.local>
2022-06-26 02:26:24 -0400

    initial commit 5

--- src/components/BCwallet.js
+++ src/components/BCwallet.js
@@ -1,27 +1,34 @@
 //import React, { useState, useEffect } from 'react';
 import React from 'react';
-import axios from 'axios';
+//import axios from 'axios';
 
 const BCwallet = () => {
 
     //const [mnemonic, setMnemonic] = useState('');
     //const [xpub, setXpubc] = useState('');
 
-    const post = async() => {
-        const res = await axios.get({
-            //baseURL: 'https://api-eu1.tatum.io/v3',
-            url: 'https://api-us-west1.tatum.io/v3/bitcoin/wallet',   
-            headers: {
-                'accept': 'application/json',
-                'x-api-key': '4845ff3d-6595-4651-a58f-48dd1d68dc0c',
-            },
-        })
-        console.log(res)
-    };
 
+
+    const query = new URLSearchParams({mnemonic: 'string'}).toString();
+
+    const post2 = async() => {
+        const resp = await fetch(
+        `https://api-us-west1.tatum.io/v3/bitcoin/wallet?${query}`,
+        {
+            method: 'GET',
+            headers: {
+            'x-api-key': '4845ff3d-6595-4651-a58f-48dd1d68dc0c'
+            }
+        }
+        );
+        const data = await resp.text();
+        console.log(data);
+    }
+
+    
     return (
         <div>
-            <button onClick={post}>
+            <button onClick={post2}>
                 Enable
             </button>
         </div>
